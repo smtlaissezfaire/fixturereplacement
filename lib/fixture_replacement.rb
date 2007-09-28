d7e536ade48b488073040817fab0171a00e552ce
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-09-28 21:42:10 +0000

    Moving around the require lines so that the specs can be run
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement@8 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement.rb
+++ lib/fixture_replacement.rb
@@ -2,12 +2,12 @@
 if ENV["RAILS_ENV"] == "test" || ENV["RAILS_ENV"] == "development"
   begin
     require File.dirname(__FILE__) + "/../../../../db/example_data"
-    require File.dirname(__FILE__) + "/fixture_replacement/string"
-    require File.dirname(__FILE__) + "/fixture_replacement/fixture_replacement"
-    
-    FixtureReplacement::Generator.generate_methods
   rescue Exception => e
     raise "Error in FixtureReplacement Plugin: #{e}"
   end
 end
 
+require File.dirname(__FILE__) + "/fixture_replacement/string"
+require File.dirname(__FILE__) + "/fixture_replacement/fixture_replacement"
+
+FixtureReplacement::Generator.generate_methods
