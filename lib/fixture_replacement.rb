4d8128f3baaef1cc7d0f1632c9f540a7dba02309
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-09-28 21:39:00 +0000

    (hopeful) bugfix (without regression) for linoj's bug
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement@7 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement.rb
+++ lib/fixture_replacement.rb
@@ -1,3 +1,13 @@
 
-require File.dirname(__FILE__) + "/fixture_replacement/string"
-require File.dirname(__FILE__) + "/fixture_replacement/fixture_replacement"
+if ENV["RAILS_ENV"] == "test" || ENV["RAILS_ENV"] == "development"
+  begin
+    require File.dirname(__FILE__) + "/../../../../db/example_data"
+    require File.dirname(__FILE__) + "/fixture_replacement/string"
+    require File.dirname(__FILE__) + "/fixture_replacement/fixture_replacement"
+    
+    FixtureReplacement::Generator.generate_methods
+  rescue Exception => e
+    raise "Error in FixtureReplacement Plugin: #{e}"
+  end
+end
+
