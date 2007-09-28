61c3fa94dc06fa7c727a6ab262bc9de8a92a446b
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-09-28 03:31:40 +0000

    removing branches and tags
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement@3 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- tags/rel-1.0/lib/fixture_replacement.rb
+++
@@ -1,3 +0,0 @@
-
-require File.dirname(__FILE__) + "/fixture_replacement/string"
-require File.dirname(__FILE__) + "/fixture_replacement/fixture_replacement"
--- tags/rel-1.0/tasks/generic_fixtures_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :fixture_replacement do
-#   # Task goes here
-# end
