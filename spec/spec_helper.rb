0b8d5144373efe384bfec7270fd263cb64813102
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-10-26 03:23:56 +0000

    adding patch from wincent to silence migrations
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement@72 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require 'active_record'
 require 'active_support'
 
 ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
+ActiveRecord::Migration.verbose = false
 
 ActiveRecord::Schema.define do
   
