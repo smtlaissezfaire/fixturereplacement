af52d8981995204a76f2eeb562ebf11055b67f53
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-12-01 06:19:52 +0000

    adding some comments
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@146 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement/delayed_evaluation_proc.rb
+++ lib/fixture_replacement/delayed_evaluation_proc.rb
@@ -1,1 +1,4 @@
+# This is here so that if someone (some how) assigns a proc
+# to an accessor on an ActiveRecord object, FixtureReplacement
+# wont' get tripped up, and try to evaluate the proc.
 class DelayedEvaluationProc < Proc; end
