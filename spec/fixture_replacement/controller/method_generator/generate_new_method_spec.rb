54826a150d82465ba9dd735a817febbefd1af6b8
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-12-01 21:00:12 +0000

    moving around some commented out specs
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@164 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- spec/fixture_replacement/controller/method_generator/generate_new_method_spec.rb
+++ spec/fixture_replacement/controller/method_generator/generate_new_method_spec.rb
@@ -79,5 +79,76 @@ module FixtureReplacementController
     it_should_behave_like "MethodGenerator#generate_new_method"
   end
 
-  
+
+
+  describe MethodGenerator, "generate_new_method for User when user_attributes is defined" do
+
+    #before :each do
+    #  @user = User.new
+    #
+    #  FixtureReplacement.module_eval do
+    #    def user_attributes
+    #      {
+    #        :gender => default_gender
+    #      }
+    #    end
+    #
+    #    def gender_attributes
+    #      {
+    #        :sex => "Male"
+    #      }
+    #    end
+    #
+    #    def alien_attributes
+    #      {
+    #        :gender => default_gender(:sex => "unknown")
+    #      }
+    #    end
+    #  end      
+    #
+    #  @gender_generator = MethodGenerator.new({:method_base_name => "gender"})
+    #  @gender_generator.generate_default_method
+    #  @gender_generator.generate_new_method
+    #  @gender_generator.generate_create_method
+    #
+    #  @generator = MethodGenerator.new({:method_base_name => "user"})
+    #  @generator.generate_new_method
+    #
+    #  @generator = MethodGenerator.new({:method_base_name => "alien"})
+    #  @generator.generate_new_method
+    #end
+    #
+    #it "should evaluate any of the default_* methods before returning (if no over-writing key is given)" do      
+    #  new_gender = new_user.gender
+    #  new_gender.sex.should == "Male"
+    #end
+    #
+    #it %(should evaluate any of the default_* methods before returning, with the hash params given to default_* method) do
+    #  new_gender = new_alien.gender
+    #  new_gender.sex.should == "unknown"
+    #end
+    #
+    #it "should call Gender.save! when the default_gender method is evaluated by default_gender" do
+    #  @gender = mock('Gender', :null_object => true)
+    #  Gender.stub!(:new).and_return @gender
+    #  @user = mock('User')
+    #  @user.stub!(:gender=).and_return @gender
+    #  User.stub!(:new).and_return @user
+    #
+    #  @gender.should_receive(:save!)
+    #  new_user
+    #end
+    #
+    #it "should not call Gender.save! if the default_gender is overwritten by another value" do
+    #  Gender.should_not_receive(:save!)
+    #  new_user(:gender => Gender.new)
+    #end
+    #
+    #it "should be able to overwrite a default_* method" do
+    #  Gender.should_not_receive(:save!)
+    #  new_user(:gender => Gender.create!(:sex => "Female"))
+    #end
+  end
 end
+
+
