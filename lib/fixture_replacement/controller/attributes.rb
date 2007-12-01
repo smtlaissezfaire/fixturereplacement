c09fea1b8777b0b2e011d47fc7b13bc6303c1f12
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2007-12-01 06:17:57 +0000

    filling out FixtureReplacementController::Attributes
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@145 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement/controller/attributes.rb
+++ lib/fixture_replacement/controller/attributes.rb
@@ -13,18 +13,61 @@ module FixtureReplacementController
       def clear_out_instances!
         @instances = nil
       end
+      
+      def find_by_fixture_name(arg)
+        instances.each do |instance|
+          if instance.fixture_name == arg
+            return instance
+          end
+        end
+        return nil
+      end
     end
     
     def initialize(h={})
-      h[:fixture_name] ? @fixture_name = h[:fixture_name] : raise
-      @attributes = h[:attributes] || Hash.new
+      assign_from_constructor(h)
       self.class.add_instance(self)
     end
     
     attr_reader :fixture_name
+    attr_reader :from
+
+    # I would really like to name this method class, but I'm
+    # sure you can see the name conflict
+    def of_class
+      begin
+        @class || find_by_fixture_name(@from).of_class
+      rescue
+        Object.const_get classify(fixture_name)
+      end
+    end
     
     def hash
-      @attributes.to_hash
+      @merged_hash || @attributes.to_hash
+    end
+    
+    def merge!
+      if from = find_by_fixture_name(self.from)
+        @merged_hash = from.hash.merge(self.hash)
+      end
+    end
+    
+  private
+  
+    def assign_from_constructor(hash_given)
+      hash_given[:fixture_name] ? @fixture_name = hash_given[:fixture_name] : raise
+      @attributes = hash_given[:attributes] || Hash.new
+      @from, @class = hash_given[:from], hash_given[:class]
+    end
+  
+    attr_reader :hash_given
+  
+    def find_by_fixture_name(symbol)
+      self.class.find_by_fixture_name(symbol)
+    end
+    
+    def classify(symbol)
+      symbol.to_s.classify
     end
   end
 end
--- spec/fixture_replacement/controller/attributes_spec.rb
+++ spec/fixture_replacement/controller/attributes_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + "/../../spec_helper"
 module FixtureReplacementController
   describe Attributes do
     
-    before :each do
+    after :each do
       Attributes.clear_out_instances!
     end
     
@@ -27,16 +27,54 @@ module FixtureReplacementController
       a1.fixture_name.should == :foo
     end
     
+    it "should have the from attribute as nil, if none provided" do
+      a1 = Attributes.new({:fixture_name => :foo})
+      a1.from.should be_nil
+    end
+    
+    it "should have the from attribute as the symbol of the attribute from which it derives" do
+      a1 = Attributes.new({:fixture_name => :foo, :from => :bar})
+      a1.from.should == :bar
+    end
+    
     it "should raise an error if no fixture_name is given" do
       lambda {
         Attributes.new({})
       }.should raise_error
     end
+    
+    it "should be able to find the Attribute by fixture name" do
+      a = Attributes.new({:fixture_name => :baz})
+      Attributes.find_by_fixture_name(:baz).should == a
+    end
+    
+    it "should find no attributes for fixture_name :baz, if it was never created" do
+      a = Attributes.new({:fixture_name => :bar})
+      Attributes.find_by_fixture_name(:baz).should be_nil
+    end
+    
+    it "should find no attributes for fixture_name :baz, if no fixture at all was ever created" do
+      Attributes.find_by_fixture_name(:baz).should be_nil
+    end
+    
+    it "should have the class name if specified" do
+      Attributes.new({:fixture_name => :foo, :class => Object}).of_class.should == Object
+    end
+    
+    it "should use the class name of the fixture_name, camel-cased, if the class is unspecified, and the fixture uninherited" do
+      Attributes.new(:fixture_name => :object).of_class.should == Object
+    end
+    
+    it "should use the class name of the inherited attribute, if specified" do
+      Attributes.new({:fixture_name => :foo, :class => Object})
+      Attributes.new({:fixture_name => :bar, :from => :foo}).of_class.should == Object      
+    end    
   end  
   
-  describe Attributes, "hash, with simple arguments" do
-    before :each do
-      
+  describe Attributes, "hash, with simple arguments (only attributes and fixture name)" do
+    
+    after :each do
+      Attributes.clear_out_instances!
     end
     
     it "should return a hash" do
@@ -53,4 +91,76 @@ module FixtureReplacementController
       }
     end
   end
+  
+  module AttributeFromHelper
+    def setup_attributes
+      @from_attributes_as_struct = OpenStruct.new({:first_name => :scott})
+      @from_attributes = Attributes.new({
+        :fixture_name => :foo,
+        :attributes => @from_attributes_as_struct
+      })
+    end
+  end
+  
+  describe Attributes, "with an empty hash, after merge with another inherited attribute" do    
+    include AttributeFromHelper
+    
+    before :each do
+      setup_attributes
+      @attributes = Attributes.new({
+        :fixture_name => :bar,
+        :from => :foo
+      })
+    end
+    
+    after :each do
+      Attributes.clear_out_instances!
+    end
+
+    it "should contain the keys from the inherited hash only" do
+      @attributes.merge!
+      @attributes.hash.should == {
+        :first_name => :scott
+      }
+    end
+  end
+  
+  describe Attributes, "with a hash, after merge with another inherited attributes" do
+    include AttributeFromHelper
+    
+    before :each do
+      setup_attributes      
+    end
+    
+    after :each do
+      Attributes.clear_out_instances!
+    end
+    
+    it "should overwrite an attribute" do
+      open_struct = OpenStruct.new({:first_name => :scott})
+      
+      attributes = Attributes.new({
+        :fixture_name => :bar,
+        :from => :foo,
+        :attributes => open_struct
+      })
+      
+      attributes.merge!
+      attributes.hash.should == {:first_name => :scott}
+    end
+    
+    it "should keep any new attributes, as well as any attributes which weren't overwritten" do
+      open_struct = OpenStruct.new({:foo => :bar})
+      
+      attributes = Attributes.new({
+        :fixture_name => :bar,
+        :from => :foo,
+        :attributes => open_struct
+      })
+      
+      attributes.merge!
+      attributes.hash.should == {:foo => :bar, :first_name => :scott}      
+    end
+  end
+  
 end
