e7b2ea26bf7afebebd252aeb0ad3b0cdc63b6721
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-01-13 00:57:35 +0000

    adding a comment
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@256 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement/class_methods.rb
+++ lib/fixture_replacement/class_methods.rb
@@ -37,6 +37,10 @@ module FixtureReplacement
       call_after_include_if_exists
     end
     
+    # Any user defined instance methods (as well as default_*) need the module's class scope to be
+    # accessible inside the block given to attributes_for
+    #
+    # Addresses bug #16858 (see CHANGELOG)
     def method_added(method)
       module_function method if method != :method_added
     end
