165dd6d03903516ea32ea82b42fa0ecd0f5c1a27
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-01-13 01:35:38 +0000

    beefing up test coverage
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@258 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- spec/fixture_replacement/integration/attributes_from_spec_without_block.rb
+++ spec/fixture_replacement/integration/attributes_from_spec_without_block.rb
@@ -1,35 +1,74 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
-module FixtureReplacementController
-  describe Attributes do
-    before :each do
-      @module = Module.new do
-        class << self
-          include FixtureReplacement::ClassMethods
-        end
-        
-        attributes_for :user do |u|
-          u.username = random_string
-          u.key = String.random
-        end
 
-        attributes_for :scott, :from => :user
-        
-        attributes_for :foo, :class => User
-        
-        attributes_for :admin do |a|
-          a.admin_status = true
-        end
-        
-      private
+module FixtureReplacementControllerHelper
+  def setup_fixtures
+    @module = Module.new do
+      class << self
+        include FixtureReplacement::ClassMethods
+      end
       
-        def random_string
-          String.random
-        end
+      attributes_for :user do |u|
+        u.username = random_string
+        u.key = String.random
       end
 
-      FixtureReplacementController::MethodGenerator.generate_methods(@module)
-      self.class.send :include, @module
+      attributes_for :scott, :from => :user
+      
+      attributes_for :foo, :class => User
+      
+      attributes_for :admin do |a|
+        a.admin_status = true
+      end
+      
+    private
+    
+      def random_string
+        String.random
+      end
+    end
+
+    FixtureReplacementController::MethodGenerator.generate_methods(@module)
+    self.class.send :include, @module
+  end
+end
+
+module FixtureReplacementController
+  
+  
+  describe Attributes do
+    include FixtureReplacementControllerHelper
+    
+    before :each do
+      setup_fixtures
+    end
+    
+    models = "user", "foo", "scott"
+    
+    models.each do |model|
+      it "should have the default_#{model} as a (module) method on the module" do
+        @module.should respond_to("default_#{model}")
+      end
+      
+      it "should have the default_#{model} as a private method in the test case" do
+        self.private_methods.should include("default_#{model}")
+      end
+      
+      it "should have the new_#{model} method as a (module) method on the module" do
+        @module.should respond_to("new_#{model}")
+      end
+      
+      it "should have the new_#{model} method as a private method" do
+        self.private_methods.should include("new_#{model}")
+      end 
+
+      it "should have the create_#{model} method as a private method" do
+        self.private_methods.should include("create_#{model}")
+      end
+      
+      it "should have the create_#{model} method as a (module) method on the module" do
+        @module.should respond_to("create_#{model}")
+      end
     end
     
     it "should have the username as a string (for User) for new_user" do
@@ -40,28 +79,5 @@ module FixtureReplacementController
       create_user.username.class.should == String
     end
     
-    it "should have the new_user method as a private method" do
-      self.private_methods.should include("new_user")
-    end 
-
-    it "should have the create_user method as a private method" do
-      self.private_methods.should include("create_user")
-    end 
-
-    it "should have the new_foo method as a private method" do
-      self.private_methods.should include("new_foo")
-    end 
-
-    it "should have the create_foo method as a private method" do
-      self.private_methods.should include("create_foo")
-    end 
-
-    it "should have the new_scott method as a private method" do
-      self.private_methods.should include("new_scott")
-    end 
-
-    it "should have the create_scott method as a private method" do
-      self.private_methods.should include("create_scott")
-    end 
   end
 end
