77d448b4e070e5926291aef8a21aa3476aecb09f
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-02-18 07:58:48 +0000

    more refactoring
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@319 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/fixture_replacement/controller/active_record_factory.rb
+++ lib/fixture_replacement/controller/active_record_factory.rb
@@ -10,44 +10,37 @@ module FixtureReplacementController
       @hash_given_to_constructor = hash
       @caller = original_caller
     end
-
-    def to_created_instance
-      created_obj = self.to_new_instance
-      created_obj.save!
-      created_obj
-    end
     
-    def hash_given_to_constructor
-      @hash_given_to_constructor || Hash.new
-    end
-
     def to_new_instance
       new_object = @attributes.of_class.new
       assign_values_to_instance new_object
       return new_object
     end
     
+    def to_created_instance
+      created_obj = self.to_new_instance
+      created_obj.save!
+      return created_obj
+    end
+
+  protected
+  
+    def hash_given_to_constructor
+      @hash_given_to_constructor || Hash.new
+    end
+    
   private
 
-    def all_attributes
-      @attributes.merge!
-      @all_merged_attributes ||= attributes_hash.merge(self.hash_given_to_constructor)
-    end
-  
-    def attributes_hash
-      @attributes.hash
+    def assign_values_to_instance(instance_object)
+      all_attributes.each do |key, value|
+        value = evaluate_possible_delayed_proc(value)
+        instance_object.__send__("#{key}=", value)             
+      end
     end
 
     def find_value_from_delayed_evaluation_proc(value)
       default_obj, params = value.call
       value = @caller.__send__("create_#{default_obj.fixture_name}", params)
-    end
-
-    def assign_values_to_instance instance_object
-      all_attributes.each do |key, value|
-        value = evaluate_possible_delayed_proc(value)
-        instance_object.__send__("#{key}=", value)             
-      end
     end
     
     def evaluate_possible_delayed_proc(value)
@@ -60,5 +53,15 @@ module FixtureReplacementController
         value
       end
     end
+    
+    def all_attributes
+      @attributes.merge!
+      @all_merged_attributes ||= attributes_hash.merge(self.hash_given_to_constructor)
+    end
+    
+    def attributes_hash
+      @attributes.hash
+    end
+    
   end
 end
