bda74b870c62679f8c90f1aceb3d1cf0985f91d2
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-02-18 07:48:38 +0000

    Moving back to an in memory sqlite database
    git-svn-id: http://thmadb.com/public_svn/plugins/fixture_replacement2@317 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- spec/spec_helpers.rb
+++ spec/spec_helpers.rb
@@ -25,7 +25,7 @@ module SpecHelperFunctions
     require 'active_record'
     require 'active_support'
     
-    ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => 'test.db'
+    ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
     ActiveRecord::Migration.verbose = false
 
     ActiveRecord::Schema.define do  
