f032c42e170e62273f24e43f3b2fdb6207229661
Scott Taylor <scott@railsnewbie.com>
2008-07-21 01:36:27 -0400

    * extend instead of including into the metaclass

--- lib/fixture_replacement/fixture_replacement.rb
+++ lib/fixture_replacement/fixture_replacement.rb
@@ -1,11 +1,7 @@
 module FixtureReplacement
   class InclusionError < StandardError; end
-  
   autoload :ClassMethods, File.dirname(__FILE__) + "/class_methods"
-  
-  class << self
-    include FixtureReplacement::ClassMethods
-  end
+  extend ClassMethods
 end
 
 
