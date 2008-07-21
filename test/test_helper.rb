fd2667500c20a13e80467571061063cf36b86343
Scott Taylor <scott@railsnewbie.com>
2008-07-21 03:06:28 -0400

    * Remove unnecessary lines from the test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,11 @@
 
-require 'rubygems'
-require 'sqlite3'
-require 'active_record'
-require 'active_support'
-
 require 'test/unit'
+
 require File.dirname(__FILE__) + "/../spec/spec_helpers"
+
 include SpecHelperFunctions
 
 setup_database_connection
 
 require File.dirname(__FILE__) + "/../lib/fixture_replacement"
 require File.dirname(__FILE__) + "/../spec/fixture_replacement/fixtures/classes"
-
-class Test::Unit::TestCase
-end
