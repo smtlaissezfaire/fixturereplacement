d3504ad308fe70735100d2292e0fd2cd1a7007dc
Scott Taylor <scott@railsnewbie.com>
2008-07-21 22:29:15 -0400

    * Cache classes in the class factory (no need to look them up   over and over again)

--- lib/fixture_replacement/controller/class_factory.rb
+++ lib/fixture_replacement/controller/class_factory.rb
@@ -2,27 +2,27 @@ module FixtureReplacementController
   module ClassFactory
     class << self
       def active_record_factory
-        ActiveRecordFactory
+        @active_record_factory ||= ActiveRecordFactory
       end
 
       def method_generator
-        MethodGenerator
+        @method_generator ||= MethodGenerator
       end
     
       def attribute_collection
-        AttributeCollection
+        @attribute_collection ||= AttributeCollection
       end
     
       def fixture_replacement_module
-        ::FixtureReplacement
+        @fixture_replacement ||= ::FixtureReplacement
       end
     
       def delayed_evaluation_proc
-        FixtureReplacementController::DelayedEvaluationProc
+        @delayed_evaluation_proc ||= FixtureReplacementController::DelayedEvaluationProc
       end
     
       def fake_active_record_instance
-        OpenStruct
+        @fake_active_record_instance ||= OpenStruct
       end
     end
   end
