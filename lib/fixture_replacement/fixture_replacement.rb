13a384be44125041d31594f08d8e53c7ed2baabf
Scott Taylor <scott@railsnewbie.com>
2008-07-21 22:27:38 -0400

    * Spacing

--- lib/fixture_replacement/fixture_replacement.rb
+++ lib/fixture_replacement/fixture_replacement.rb
@@ -1,7 +1,9 @@
 module FixtureReplacement
   class InclusionError < StandardError; end
+  
   autoload :ClassMethods, File.dirname(__FILE__) + "/class_methods"
   autoload :Version,      File.dirname(__FILE__) + "/version"
+  
   extend ClassMethods
 end
 
