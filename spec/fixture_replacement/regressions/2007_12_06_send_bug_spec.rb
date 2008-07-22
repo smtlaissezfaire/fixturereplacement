ddf48ba99b57f8849c5a9b4d80a6f382e1f19969
Scott Taylor <scott@railsnewbie.com>
2008-07-21 22:33:26 -0400

    * Silence warnings for specs which stub __send__ (redefining   __send__ causes ruby to issue a warning)

--- spec/fixture_replacement/regressions/2007_12_06_send_bug_spec.rb
+++ spec/fixture_replacement/regressions/2007_12_06_send_bug_spec.rb
@@ -16,45 +16,48 @@ module FixtureReplacementController
     end
     
     it "should be able to send the message (to_new_class_instance), even if the send method has been redefined" do
-      @instance.should_not_receive(:send)
-      @instance.should_receive(:__send__)
-      
-      @attributes.to_new_class_instance
+      silence_warnings do
+        @instance.should_not_receive(:send)
+        @instance.should_receive(:__send__)
+        
+        @attributes.to_new_class_instance
+      end
     end
   end
   
   describe AttributeCollection, "send regression, part 2" do
     before :each do
-      @class = Class.new
-      @instance = @class.new
-      @class.stub!(:new).and_return @instance
-      
-      @instance.stub!(:foo=)
-      
-      @attributes = AttributeCollection.new(:foo, {})
-      @attributes.stub!(:active_record_class).and_return @class
+      silence_warnings do
+        @class = Class.new
+        @instance = @class.new
+        @class.stub!(:new).and_return @instance
+        
+        @instance.stub!(:foo=)
+        
+        @attributes = AttributeCollection.new(:foo, {})
+        @attributes.stub!(:active_record_class).and_return @class
 
-      @mock_who_cares = mock 'who cares'
-      @mock_who_cares.stub!(:fixture_name)
+        @mock_who_cares = mock 'who cares'
+        @mock_who_cares.stub!(:fixture_name)
 
-      @value = DelayedEvaluationProc.new do
-        @mock_who_cares
+        @value = DelayedEvaluationProc.new do
+          @mock_who_cares
+        end
+        
+        @attributes.stub!(:hash).and_return({:foo => @value})
+        
+        @caller = Object.new
+        @caller.stub!(:send).and_raise
+        @caller.stub!(:__send__)
       end
-      
-      @attributes.stub!(:hash).and_return({:foo => @value})
-      
-      @caller = Object.new
-      @caller.stub!(:send).and_raise
-      @caller.stub!(:__send__)
-
     end
     
     it "should be able to send the message with __send__" do
-      @caller.should_not_receive(:send)
-      @caller.should_receive(:__send__)
-
-      @attributes.to_new_class_instance({}, @caller)
+      silence_warnings do
+        @caller.should_not_receive(:send)
+        @caller.should_receive(:__send__)
+        @attributes.to_new_class_instance({}, @caller)
+      end
     end
-
   end 
 end
