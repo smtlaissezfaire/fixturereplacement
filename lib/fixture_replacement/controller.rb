bd6cc9d86c1c54bb62dfe27eb6de533d53c23a49
Scott Taylor <scott@railsnewbie.com>
2008-09-06 03:31:03 -0400

    * Remove ClassFactory

--- lib/fixture_replacement/controller.rb
+++ lib/fixture_replacement/controller.rb
@@ -3,7 +3,6 @@ module FixtureReplacementController
   
   autoload :ActiveRecordFactory,   "#{dir}/active_record_factory"
   autoload :AttributeCollection,   "#{dir}/attribute_collection"
-  autoload :ClassFactory,          "#{dir}/class_factory"
   autoload :DelayedEvaluationProc, "#{dir}/delayed_evaluation_proc"
   autoload :MethodGenerator,       "#{dir}/method_generator"
   
