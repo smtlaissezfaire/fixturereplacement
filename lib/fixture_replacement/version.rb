9fe49c25ee0e7d6064d7d701a65c597bc7a49b72
Scott Taylor <scott@railsnewbie.com>
2008-09-06 03:32:41 -0400

    * Update version info

--- lib/fixture_replacement/version.rb
+++ lib/fixture_replacement/version.rb
@@ -7,7 +7,7 @@ module FixtureReplacement
     MAJOR    = 2
     MINOR    = 8
     TINY     = 0
-    REVISION = 'c8de53df9c72cd620f1de264db6e81faed0e225a'
+    REVISION = 'bd6cc9d86c1c54bb62dfe27eb6de533d53c23a49'
     
     VERSION  = "#{MAJOR}.#{MINOR}.#{TINY} (git #{REVISION})"
   end
