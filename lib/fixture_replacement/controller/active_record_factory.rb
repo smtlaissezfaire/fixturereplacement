ef21702a526b621ce8343d7746ce796d8739cc7c
Scott Taylor <scott@railsnewbie.com>
2008-09-06 02:24:03 -0400

    * Move Evaluator into ActiveRecordValueAssigner

--- lib/fixture_replacement/controller/active_record_factory.rb
+++ lib/fixture_replacement/controller/active_record_factory.rb
@@ -23,18 +23,44 @@ module FixtureReplacementController
       created_obj
     end
     
-    
     class ActiveRecordValueAssigner
-      def self.assign(object, key, value)
-        new(object).assign(key, value)
+      def self.assign(object, key, value, context)
+        new(object).assign(key, value, context)
       end
       
       def initialize(object)
         @object = object
       end
       
-      def assign(key, value)
-        @object.__send__("#{key}=", value)
+      def assign(key, value, context)
+        @object.__send__("#{key}=", eval(value, context))
+      end
+      
+    private
+      
+      class Evaluator
+        def self.evaluate(value, context)
+          new(value).evaluate(context)
+        end
+        
+        def initialize(value)
+          @value = value
+        end
+        
+        def evaluate(context)
+          case @value
+          when Array
+            @value.map! { |element| self.class.evaluate(element, context) }
+          when DelayedEvaluationProc
+            @value.evaluate(context)
+          else
+            @value
+          end
+        end
+      end
+      
+      def eval(value, context)
+        Evaluator.evaluate(value, context)
       end
     end
 
@@ -48,36 +74,10 @@ module FixtureReplacementController
 
     def assign_values_to_instance(instance_object)
       all_attributes.each do |key, value|
-        value = evaluate_possible_delayed_proc(value)
-        ActiveRecordValueAssigner.assign(instance_object, key, value)
+        ActiveRecordValueAssigner.assign(instance_object, key, value, @caller)
       end
     end
 
-    def evaluate_possible_delayed_proc(value)
-      Evaluator.evaluate(value, @caller)
-    end
-    
-    class Evaluator
-      def self.evaluate(value, context)
-        new(value).evaluate(context)
-      end
-      
-      def initialize(value)
-        @value = value
-      end
-      
-      def evaluate(context)
-        case @value
-        when Array
-          @value.map! { |element| self.class.evaluate(element, context) }
-        when DelayedEvaluationProc
-          @value.evaluate(context)
-        else
-          @value
-        end
-      end
-    end
-    
     def all_attributes
       @attributes.merge!
       @all_merged_attributes ||= attributes_hash.merge(self.hash_given_to_constructor)
