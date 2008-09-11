9b294feb1cc33961ef63717989ae439f2884203d
Scott Taylor <scott@railsnewbie.com>
2008-09-11 03:55:59 -0400

    * Add some comments

--- lib/fixture_replacement/fixture_replacement.rb
+++ lib/fixture_replacement/fixture_replacement.rb
@@ -6,6 +6,8 @@ module FixtureReplacement
   class InclusionError < BaseError;     end
   class UnknownFixture < BaseError;     end
   
+  # Pass FR::UNDEFINED as a value to prevent FixtureReplacement
+  # from assigning the value from example_data.rb
   UNDEFINED = FixtureReplacementController::UndefinedValue.new
   
   autoload :ClassMethods, File.dirname(__FILE__) + "/class_methods"
