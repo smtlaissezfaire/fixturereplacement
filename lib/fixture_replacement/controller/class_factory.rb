460931fca846938c4e1d53cf1d78ecd516e9e925
Scott Taylor <scott@railsnewbie.com>
2009-03-29 21:47:43 -0400

    Remove FixtureReplacementController::attribute_collection (it isn't used)

--- lib/fixture_replacement/controller/class_factory.rb
+++ lib/fixture_replacement/controller/class_factory.rb
@@ -1,10 +1,6 @@
 module FixtureReplacementController
   module ClassFactory
     class << self
-      def attribute_collection
-        AttributeCollection
-      end
-    
       def fixture_replacement_module
         ::FixtureReplacement
       end
