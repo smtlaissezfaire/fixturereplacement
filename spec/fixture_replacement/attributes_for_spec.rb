4d9696485b9a20048ecc81ffa57d6990ba4a3709
Scott Taylor <scott@railsnewbie.com>
2009-03-30 01:46:31 -0400

    Cleanup specs

--- spec/fixture_replacement/attributes_for_spec.rb
+++ spec/fixture_replacement/attributes_for_spec.rb
@@ -4,9 +4,6 @@ describe FixtureReplacement, "attributes_for" do
   before :each do
     @fixture_attribute = FixtureReplacementController::AttributeCollection
     @fixture_attribute.stub!(:new)
-    
-    @attributes_proc = lambda {}
-    Kernel.stub!(:lambda).and_return @attributes_proc
   end
   
   it "should take a fixture name" do
@@ -19,17 +16,17 @@ describe FixtureReplacement, "attributes_for" do
   
   it "should create a new FixtureAttribute with the name given" do
     @fixture_attribute.should_receive(:new).with(:foo, hash_including)
-    FixtureReplacement.attributes_for(:foo, {}, &@attributes_proc) 
+    FixtureReplacement.attributes_for(:foo, {}) 
   end
   
   it "should create a new FixtureAttribute with the name given and class given" do
     @fixture_attribute.should_receive(:new).with(:foo, hash_including(:class => Object))
-    FixtureReplacement.attributes_for(:foo, {:class => Object}, &@attributes_proc)
+    FixtureReplacement.attributes_for(:foo, {:class => Object})
   end
   
   it "should create a new FixtureAttribute with the name given and the attributes from the block" do
     @fixture_attribute.should_receive(:new).with(:foo, hash_including(:from => :bar))
-    FixtureReplacement.attributes_for(:foo, {:from => :bar}, &@attributes_proc)
+    FixtureReplacement.attributes_for(:foo, {:from => :bar})
   end
   
   it "should not raise an error if no block is given" do
