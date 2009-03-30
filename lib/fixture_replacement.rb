7a744d76054b70e2c8de63b86bd9c6d26608b900
Scott Taylor <scott@railsnewbie.com>
2009-03-30 00:47:43 -0400

    Use require_dependency instead of 'require'

--- lib/fixture_replacement.rb
+++ lib/fixture_replacement.rb
@@ -1,12 +1,12 @@
-require 'ostruct'
+require_dependency 'ostruct'
 
 dir = File.dirname(__FILE__) + "/fixture_replacement"
-require "#{dir}/class_methods"
-require "#{dir}/controller/active_record_factory"
-require "#{dir}/controller/attribute_collection"
-require "#{dir}/controller/delayed_evaluation_proc"
-require "#{dir}/controller/method_generator"
-require "#{dir}/extensions/string"
+require_dependency "#{dir}/class_methods"
+require_dependency "#{dir}/controller/active_record_factory"
+require_dependency "#{dir}/controller/attribute_collection"
+require_dependency "#{dir}/controller/delayed_evaluation_proc"
+require_dependency "#{dir}/controller/method_generator"
+require_dependency "#{dir}/extensions/string"
 
 module FixtureReplacement
   extend FixtureReplacement::ClassMethods
