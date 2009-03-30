55b87d03b70f0cfc8e7b62bcdb7e9ded4f695e85
Scott Taylor <scott@railsnewbie.com>
2009-03-30 00:41:28 -0400

    Move to a list of static requires

--- lib/fixture_replacement.rb
+++ lib/fixture_replacement.rb
@@ -23,10 +23,13 @@
 
 require 'ostruct'
 
-files = Dir.glob(File.dirname(__FILE__) + "/fixture_replacement/**/*.rb")
-files.each do |file|
-  require file
-end
+dir = File.dirname(__FILE__) + "/fixture_replacement"
+require "#{dir}/class_methods"
+require "#{dir}/controller/active_record_factory"
+require "#{dir}/controller/attribute_collection"
+require "#{dir}/controller/delayed_evaluation_proc"
+require "#{dir}/controller/method_generator"
+require "#{dir}/extensions/string"
 
 module FixtureReplacement
   extend FixtureReplacement::ClassMethods
