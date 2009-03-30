418dfa680728514640ec124a161a257bc50516f5
Scott Taylor <scott@railsnewbie.com>
2009-03-30 03:33:44 -0400

    Add some integration specs for default_*

---
+++ spec/fixture_replacement/integration/default_method_spec.rb
@@ -0,0 +1,43 @@
+require File.dirname(__FILE__) + "/../../spec_helper"
+
+module FixtureReplacementController
+  describe "default_method" do
+    def use_module(&block)
+      mod = Module.new
+      mod.extend(FixtureReplacement::ClassMethods)
+      mod.instance_eval(&block)
+      
+      FixtureReplacementController::MethodGenerator.generate_methods(mod)
+      extend mod
+    end
+    
+    it "should create a proc" do
+      use_module do
+        attributes_for :user
+      end
+      
+      default_user.should be_a_kind_of(Proc)
+    end
+    
+    it "should instantiate a new instance of the class when calling the proc generated" do
+      use_module do
+        attributes_for :user do |u|
+          u.key = :foo
+        end
+      end
+      
+      default_user.evaluate(self).should be_a_kind_of(User)
+    end
+    
+    it "should create the object" do
+      use_module do
+        attributes_for :user do |u|
+          u.key = :foo
+        end
+      end
+      
+      obj = default_user.evaluate(self)
+      obj.should_not be_a_new_record
+    end
+  end
+end
