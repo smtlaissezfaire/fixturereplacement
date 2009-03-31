fb307ea801965dbce271478a247ebca47f6a5e4b
Scott Taylor <scott@railsnewbie.com>
2009-03-31 00:02:25 -0400

    Move files out of the controller directory

--- lib/fixture_replacement.rb
+++ lib/fixture_replacement.rb
@@ -2,9 +2,9 @@ require 'ostruct'
 
 dir = File.dirname(__FILE__) + "/fixture_replacement"
 load "#{dir}/class_methods.rb"
-load "#{dir}/controller/active_record_factory.rb"
-load "#{dir}/controller/attribute_builder.rb"
-load "#{dir}/controller/method_generator.rb"
+load "#{dir}/active_record_factory.rb"
+load "#{dir}/attribute_builder.rb"
+load "#{dir}/method_generator.rb"
 load "#{dir}/extensions/string.rb"
 
 module FixtureReplacement
--- spec/fixture_replacement/method_generator/evaluation_loading_spec.rb
+++ spec/fixture_replacement/method_generator/evaluation_loading_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + "/../../../spec_helper"
+require File.dirname(__FILE__) + "/../../spec_helper"
 
 module FixtureReplacement
   describe MethodGenerator, "Evaluation loading" do
