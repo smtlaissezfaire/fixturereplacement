8be159509ab9a56a2affc81566ab4e49948cc327
Scott Taylor <scott@railsnewbie.com>
2009-04-04 15:57:20 -0400

    Refactory.  Only open evaluation module (FixtureReplacement) once for every attributes_for call

--- lib/fixture_replacement/method_generator.rb
+++ lib/fixture_replacement/method_generator.rb
@@ -6,50 +6,25 @@ module FixtureReplacement
     end
     
     def generate_methods
-      generate_valid_attributes_method
-      generate_default_method
-      generate_new_method
-      generate_create_method
-    end
-    
-    def generate_valid_attributes_method
       builder = @builder
       
       @evaluation_module.module_eval do
         define_method("valid_#{builder.fixture_name}_attributes") do |*args|
           builder.to_hash(*args)
         end
-      end
-    end
-    
-    def generate_default_method
-      builder = @builder
-      
-      @evaluation_module.module_eval do
+
         define_method("default_#{builder.fixture_name}") do |*args|
           lambda do
             __send__("create_#{builder.fixture_name}", *args)
           end
         end
-      end
-    end
-    
-    def generate_create_method
-      builder = @builder
-      
-      @evaluation_module.module_eval do
+
         define_method("create_#{builder.fixture_name}") do |*args|
           obj = __send__("new_#{builder.fixture_name}", *args)
           obj.save!
           obj
         end
-      end
-    end
-    
-    def generate_new_method
-      builder = @builder
-      
-      @evaluation_module.module_eval do
+        
         define_method("new_#{builder.fixture_name}") do |*args|
           new_object = builder.active_record_class.new
           
