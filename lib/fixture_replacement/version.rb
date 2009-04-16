435fee6f056e6ff66f527390587a1a00810d28f9
Scott Taylor <scott@railsnewbie.com>
2009-04-16 03:19:59 -0400

    Set version to 2.1

--- lib/fixture_replacement/version.rb
+++ lib/fixture_replacement/version.rb
@@ -2,8 +2,8 @@ module FixtureReplacement
   module Version
     unless defined?(FixtureReplacement::VERSION)
       MAJOR = 2
-      MINOR = 0
-      TINY  = 1
+      MINOR = 1
+      TINY  = 0
 
       VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
     end
--- spec/fixture_replacement/version_spec.rb
+++ spec/fixture_replacement/version_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe FixtureReplacement do
   it "should be at version 2.0.1" do
-    FixtureReplacement::VERSION.should == "2.0.1"
+    FixtureReplacement::VERSION.should == "2.1.0"
   end
 end
