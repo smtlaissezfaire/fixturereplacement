39dd3daf80d1cf3838f4b078b08e7817dbb39e11
Scott Taylor <scott@railsnewbie.com>
2009-05-22 19:50:06 -0400

    Update to version 2.1.1

--- lib/fixture_replacement/version.rb
+++ lib/fixture_replacement/version.rb
@@ -3,7 +3,7 @@ module FixtureReplacement
     unless defined?(FixtureReplacement::VERSION)
       MAJOR = 2
       MINOR = 1
-      TINY  = 0
+      TINY  = 1
 
       VERSION = "#{MAJOR}.#{MINOR}.#{TINY}"
     end
