3d059f074b950142e088d481d19f7408672a6d86
Scott Taylor <scott@railsnewbie.com>
2009-06-02 03:00:19 -0400

    Remove stupid comments

--- lib/fixture_replacement/attribute_builder.rb
+++ lib/fixture_replacement/attribute_builder.rb
@@ -1,12 +1,4 @@
 module FixtureReplacement
-  # I am a series of ActiveRecord model attributes.
-  #
-  # My attributes come from the following places: 
-  #
-  #   * from the class which is specified with :from => :fixture_name
-  #     when I was constructed
-  #   * from the anonymous function which is passed from into my constructor
-  #
   class AttributeBuilder
     class << self
       def instances
@@ -21,10 +13,6 @@ module FixtureReplacement
         @instances = nil
       end
       
-      # Finds the fixture by the given name
-      # If there are duplicate fixtures with the same name,
-      # it will find the first one which was specified.  It will
-      # return nil if no fixture with the name given was found
       def find_by_fixture_name(arg)
         instances.detect { |instance| instance.fixture_name == arg }
       end
