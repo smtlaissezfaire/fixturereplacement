acb88d1fbf4f755aa748b46b683295275568eb11
Scott Taylor <scott@railsnewbie.com>
2022-09-07 21:04:35 -0700

    bump version: 4.0.0

--- lib/fixture_replacement/version.rb
+++ lib/fixture_replacement/version.rb
@@ -1,9 +1,9 @@
 module FixtureReplacement
   module Version
     unless defined?(FixtureReplacement::VERSION)
-      MAJOR             = 3
+      MAJOR             = 4
       MINOR             = 0
-      TINY              = 1
+      TINY              = 0
 
       version_string = "#{MAJOR}.#{MINOR}.#{TINY}"
       version_string << " RC#{RELEASE_CANDIDATE}" if defined?(RELEASE_CANDIDATE)
--- spec/fixture_replacement/version_spec.rb
+++ spec/fixture_replacement/version_spec.rb
@@ -2,6 +2,6 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe FixtureReplacement do
   it "should be at version 3.0.1" do
-    FixtureReplacement::VERSION.should == "3.0.1"
+    FixtureReplacement::VERSION.should == "4.0.0"
   end
 end
