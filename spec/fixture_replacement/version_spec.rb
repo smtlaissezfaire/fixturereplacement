4dd97d18dc70c9df61e5af47192a472ec810f8a0
Scott Taylor <scott@railsnewbie.com>
2022-09-07 21:13:24 -0700

    minor version reference fixes

--- spec/fixture_replacement/version_spec.rb
+++ spec/fixture_replacement/version_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe FixtureReplacement do
-  it "should be at version 3.0.1" do
+  it "should be at the right version" do
     FixtureReplacement::VERSION.should == "4.0.0"
   end
 end
