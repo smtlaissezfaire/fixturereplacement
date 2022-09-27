5dd3f133379291c701d76f543568f4a7b49491a2
Scott Taylor <scott@railsnewbie.com>
2022-09-27 13:12:17 -0700

    add faker helpers

--- lib/fixture_replacement/random_data_generators.rb
+++ lib/fixture_replacement/random_data_generators.rb
@@ -6,5 +6,25 @@ module FixtureReplacement
       1.upto(length) { |i| string << chars[rand(chars.size-1)]}
       string
     end
+
+    def random_email(*args)
+      require 'faker'
+      Faker::Internet.email(*args)
+    end
+
+    def random_first_name
+      require 'faker'
+      Faker::Name.first_name
+    end
+
+    def random_last_name
+      require 'faker'
+      Faker::Name.last_name
+    end
+
+    def random_name
+      require 'faker'
+      Faker::Name.name
+    end
   end
 end
